5662184003bc4ea2249f37ba338ef79dbcfe5b7e
Chia-Jui Chou <agoodmanjerry@gmail.com>
2024-05-15 05:34:52 +0800

    allowing configurations

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "nightfox", "astrodark", "habamax" } },
+  install = { colorscheme = { "astrodark", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
